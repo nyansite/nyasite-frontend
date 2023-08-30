1feeb67776c8f0a9e1e25dee7f4dce559cf15b54
tksmly <tksmly@gmail.com>
2023-08-30 18:19:27 +0800

    ico -> png

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="zh-hans">
       <body className={inter.className}>{children}</body>
     </html>
   )
