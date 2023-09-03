1e8163dc46d41f0e93a82c6ad515bd2d385402db
tksmly <tksmly@gmail.com>
2023-09-03 17:06:33 +0800

    导航栏

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <a href='/login'>登录</a>
-      <a href='/register'>注册</a>
+      <div className="flex flex-row flex-nowrap h-12 fixed top-0 left-0 right-0 justify-around bg-white ">
+        
+        <a href='/login' className="flex text-center h-full items-center">登录</a>
+        <a href='/register' className="flex text-center h-full items-center">注册</a>
+      </div>
+
     </main>
   )
 }
