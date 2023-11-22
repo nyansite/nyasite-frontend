9c72d46325e717a9b3bece8116a81ba613a490f3
tksmly <tksmly@gmail.com>
2023-11-22 16:46:49 +0800

    大量重构

--- app/page.js
+++ app/page.js
@@ -1,13 +1,6 @@
-import Navbar from "./Navbar.js"
-import Sidebar from "./Sidebar.js"
 export default function Home() {
   return (
-    <main className="gap-4">
-      <Navbar/>
-      <div className="flex">
-        <Sidebar/>
-        <div>114514</div>
-      </div>
+    <main>
     </main>
   )
 }
