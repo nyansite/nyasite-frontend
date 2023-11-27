a8b7e8f24d363feabae77bdedd90cc27adcab61a
tksmly <tksmly@gmail.com>
2023-11-27 19:38:28 +0800

    Revert "start.sh"
    This reverts commit e8db383f19a57f16b9fd30ff3ecb985987ffc4db.

--- start.sh
+++
@@ -1,8 +0,0 @@
-export PATH=$PATH:/usr/local/go/bin
-cd /nyasite/nyasiteNodeBB
-node nodebb start &
-cd /nyasite/nyasiteBackend/
-CGO_ENABLED=1 go run . &
-cd /nyasite/nyasiteFrontend
-npm install
-npm run dev
