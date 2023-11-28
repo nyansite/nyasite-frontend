7be96c3e5f9aefeb519618650397d29ac614b896
tksmly <tksmly@gmail.com>
2023-11-28 20:02:29 +0800

    prettier

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,6 @@ import SearchInput from "./SearchInput";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -15,7 +14,6 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang="zh-hans">
 			<body className={inter.className}>
-
 				<div className="flex fixed w-full bg-white h-16 top-0 items-center">
 					<a className="justify-self-start whitespace-nowrap flex items-center" href="/">
 						<img src="/logo.svg" className=" w-16 h-16" />
@@ -25,13 +23,23 @@ export default function RootLayout({ children }) {
 					</div>
 					<div className=" absolute right-0 whitespace-nowrap flex items-center">
 						<a className="img_b">
-							<svg fill="none" stroke="#516e8b" strokeWidth={1.5} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" className=" w-12 h-12 ">
-								<path strokeLinecap="round" strokeLinejoin="round" d="M8.625 9.75a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 01.778-.332 48.294 48.294 0 005.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
+							<svg
+								fill="none"
+								stroke="#516e8b"
+								strokeWidth={1.5}
+								viewBox="0 0 24 24"
+								xmlns="http://www.w3.org/2000/svg"
+								aria-hidden="true"
+								className=" w-12 h-12 "
+							>
+								<path
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									d="M8.625 9.75a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 01.778-.332 48.294 48.294 0 005.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"
+								/>
 							</svg>
 						</a>
-						<a className="img_b">
-							
-						</a>
+						<a className="img_b"></a>
 					</div>
 				</div>
 				<div className="w-full h-16"></div>
