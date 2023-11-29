55370afd0012d736e8fc9129bf4f9835563bad8c
tksmly <tksmly@gmail.com>
2023-11-29 21:01:04 +0800

    登录界面的边框

--- app/login/login.js
+++ app/login/login.js
@@ -30,46 +30,48 @@ export default function Login_c() {
 	};
 
 	return (
-		<main className="grid place-items-center h-screen">
-			<form id="flogin" onSubmit={handleSubmit} className="">
-				<div className="flex justify-center">登录</div>
-				<input
-					className="block"
-					name="username"
-					id="username"
-					placeholder="用户名或邮箱"
-					autoComplete="username"
-					required
-					autoFocus
-				/>
-				<input
-					className="block"
-					name="passwd"
-					id="passwd"
-					placeholder="密码"
-					type="password"
-					autoComplete="current-password"
-					minLength="6"
-					required
-				/>
-				<div className="flex justify-between">
-					<a href="/register">注册账户</a>
-					<a href="">重置密码</a>
-				</div>
-				<div className="flex justify-center">
-					<button type="submit">
-						<Image
-							src="/chevron-right-solid.svg"
-							alt="确定"
-							width={32}
-							height={32}
-							className="h-[2em] w-auto"
-						></Image>
-						{/* 必须指定宽高或填充,单位只能是px  https://nextjs.org/docs/app/api-reference/components/image#width
+		<main className="grid place-items-center h-screen ">
+			<div className="shadow-md shadow-black rounded border border-black p-4 ">
+				<form id="flogin" onSubmit={handleSubmit} className="">
+					<div className="flex justify-center">登录</div>
+					<input
+						className="block my-2 bg-transparent"
+						name="username"
+						id="username"
+						placeholder="用户名或邮箱"
+						autoComplete="username"
+						required
+						autoFocus
+					/>
+					<input
+						className="block my-2 bg-transparent "
+						name="passwd"
+						id="passwd"
+						placeholder="密码"
+						type="password"
+						autoComplete="current-password"
+						minLength="6"
+						required
+					/>
+					<div className="flex justify-between">
+						<a href="/register">注册账户</a>
+						<a href="">重置密码</a>
+					</div>
+					<div className="flex justify-center">
+						<button type="submit">
+							<Image
+								src="/chevron-right-solid.svg"
+								alt="确定"
+								width={32}
+								height={32}
+								className="h-[2em] w-auto"
+							></Image>
+							{/* 必须指定宽高或填充,单位只能是px  https://nextjs.org/docs/app/api-reference/components/image#width
 							随便指定一个较小的值然后用css覆盖 */}
-					</button>
-				</div>
-			</form>
+						</button>
+					</div>
+				</form>
+			</div>
 		</main>
 	);
 }
