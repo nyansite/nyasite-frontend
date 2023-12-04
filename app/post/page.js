e39c70c5c155be14b5c7d001b490fe991f6f34f3
tksmly <tksmly@gmail.com>
2023-12-04 17:56:26 +0800

    uninstall axios&install swr

--- app/post/page.js
+++ app/post/page.js
@@ -1,46 +1,6 @@
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
-import axios from "axios";
 import { PostVideo } from "./post.js";
-function get_header() {
-	const headersL = headers();
-	const JheadersList = {};
-	headersL.forEach((v, k) => (JheadersList[k] = v)); //迭代器->JSON
-	return JheadersList;
-}
-
-export async function Upload(videoFiles) {
-	"use server";
-	var postHeaders = get_header();
-	var formData = new FormData();
-	videoFiles.forEach((i) => {
-		formData.append("video", i);
-	});
-	// axios for <progress/>
-	return axios.create({
-		url: "/upload_video",
-		method: "POST",
-		baseURL: "http://localhost:8000/uapi/",
-		headers: {
-			cookie: postHeaders.cookie,
-		},
-		data: formData,
-	});
-	/*const response = await fetch("http://localhost:8000/uapi/upload_video", {
-		method: "POST",
-		body: FormData,
-		credentials: "include",
-		headers: {
-			cookie: postHeaders.cookie,
-		},
-	});
-	if (response.status==200){
-		const list = await response.json()
-		return list
-	}else{
-		return response.status
-	}*/
-}
 
 export default async function Post() {
 	const res = await fetch("http://localhost:8000/api/user_status", { headers: get_header() });
--- app/post/post.js
+++ app/post/post.js
@@ -1,17 +1,19 @@
 "use client";
 
-export function PostVideo({Upload}) {
-	
-	async function uploadVideos(files){
-		const uploadAxiosInstance = Upload(files)
-		
+export function PostVideo({ Upload }) {
+	async function uploadVideos(files) {
+		const uploadAxiosInstance = Upload(files);
 	}
-	if (step == 0) {
-		return (
-			<button className=" w-10/12 h-3/5 border rounded-xl">
-				<input id="file" accept="video/*" type="file" style={{ display: "none" }} 
-					onChange={(e) => uploadVideos(e.target.files)}/>
-			</button>
-		);
-	}
+
+	return (
+		<button className=" w-10/12 h-3/5 border rounded-xl">
+			<input
+				id="file"
+				accept="video/*"
+				type="file"
+				style={{ display: "none" }}
+				onChange={(e) => uploadVideos(e.target.files)}
+			/>
+		</button>
+	);
 }
