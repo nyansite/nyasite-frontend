f252d121631fa8aa3f57bdbca2fa4e88a84dc505
flyingsnoopy <lijiaqi1835@outlook.com>
2023-12-25 18:13:33 +0800

    title description and tags

--- app/post/page.js
+++ app/post/page.js
@@ -18,15 +18,22 @@ export default async function Post() {
 	if (res.status == 200) {
 		const list = await res.json();
 		const resPICUItoken = await fetch("http://localhost:8000/api/get_PICUI_token", { headers: get_header() })
-		if (resPICUItoken.status != 200) {
+		const resTagList = await fetch("http://localhost:8000/api/search/taglist", { headers: get_header() })
+		if (resTagList.status != 200) {
+			return (
+				<a href="/">获取标签列表出现错误</a>
+			)
+		} else if (resPICUItoken.status != 200) {
 			return (
 				<a href="/">获取图床token出现错误</a>
-			);
-		} else {
+			)
+		}else {
 			const token = await resPICUItoken.text()
+			const taglistJSON = await resTagList.json()
+			console.log(taglistJSON)
 			return (
 				<main>
-					<Post_c PICUItoken={token}/>
+					<Post_c PICUItoken={token} TagList={taglistJSON.results} />
 				</main>
 			)
 		}
--- app/post/post.js
+++ app/post/post.js
@@ -1,14 +1,20 @@
 'use client'
 import React, { useEffect, useRef, useState } from "react"
+import { render } from "react-dom"
 import { ArrowsPointingInIcon, ArrowsPointingOutIcon } from "@heroicons/react/24/outline"
+//Uppy
 import Uppy from '@uppy/core'
 import Tus from "@uppy/tus"
 import { Dashboard } from "@uppy/react"
 import '@uppy/core/dist/style.min.css'
 import '@uppy/dashboard/dist/style.min.css'
 import '@uppy/drag-drop/dist/style.min.css'
+//cropper
 import Cropper from "react-cropper"
 import "cropperjs/dist/cropper.css"
+//react-tag-input
+import { WithContext as ReactTags } from 'react-tag-input';
+//self
 import { UploadCoverFunc } from "./action.js"
 import "./post.css"
 
@@ -51,7 +57,7 @@ function UploadVideo({ GetVideoUrl }) {
 
 //uploadCover
 
-function UploadCoverContnet({ PICUItoken, GetCoverUrl,Display }) {
+function UploadCoverContnet({ PICUItoken, GetCoverUrl, Display }) {
     const [originalImg, setOriginalImg] = useState()
     const [coverImgBlob, setCoverImgBlob] = useState()
     const [uploadStatus, setUploadStatus] = useState('')
@@ -104,11 +110,11 @@ function UploadCoverContnet({ PICUItoken, GetCoverUrl,Display }) {
     }
     return (
         <>
-            <button className="text_b" onClick={getFilds} style={{display:Display?"block":"none"}}>
+            <button className="text_b" onClick={getFilds} style={{ display: Display ? "block" : "none" }}>
                 选择图片
                 <input id='file' accept="image/*" type="file" onChange={imgGet} style={{ display: "none" }} />
             </button>
-            <div className="w-full" style={{display:Display?"block":"none"}}>
+            <div className="w-full" style={{ display: Display ? "block" : "none" }}>
                 <Cropper
                     src={originalImg}
                     style={{ height: "100%", width: "100%" }}
@@ -124,7 +130,7 @@ function UploadCoverContnet({ PICUItoken, GetCoverUrl,Display }) {
                     aspectRatio={16 / 9}
                 />
             </div>
-            <div className=" flex items-center gap-2" style={{display:Display?"block":"none"}}>
+            <div className=" flex items-center gap-2" style={{ display: Display ? "block" : "none" }}>
                 <button className="text_b" onClick={uploadCover} style={{ display: originalImg ? "block" : "none" }}>上传</button>
                 <div>{uploadStatus}</div>
             </div>
@@ -153,23 +159,80 @@ function UploadCover({ PICUItoken, GetCoverUrl }) {
                     }
                 </button>
             </div>
-            <UploadCoverContnet PICUItoken={PICUItoken} GetCoverUrl={GetCoverUrl} Display={uploadCoverShow}/>
+            <UploadCoverContnet PICUItoken={PICUItoken} GetCoverUrl={GetCoverUrl} Display={uploadCoverShow} />
         </div>
     )
 }
 
+
 //body
 
-export function Post_c({ PICUItoken }) {
+export function Post_c({ PICUItoken, TagList }) {
+    //select tags
+    const [tags, setTags] = useState([])
+    const suggestions = TagList.map((tag) => {
+        return {
+            id: tag,
+            text: tag,
+        }
+    })
+    console.log(suggestions)
+    const delimiters = [180, 20]
+    const handleDelete = i => {
+        setTags(tags.filter((tag, index) => index !== i));
+    };
+
+    const handleAddition = tag => {
+        setTags([...tags, tag]);
+    };
+    const handleDrag = (tag, currPos, newPos) => {
+        const newTags = tags.slice();
+
+        newTags.splice(currPos, 1);
+        newTags.splice(newPos, 0, tag);
+
+        // re-render
+        setTags(newTags);
+    };
+
+    const handleTagClick = index => {
+        console.log('The tag at index ' + index + ' was clicked');
+    };
+    //
     const [coverUrl, setCoverUrl] = useState('')
+
     return (
         <main className="flex flex-col w-full items-center">
             <div className="flex flex-col w-7/12 items-center gap-6">
                 <UploadVideo />
                 <UploadCover PICUItoken={PICUItoken} GetCoverUrl={setCoverUrl} />
-                <form className="flex flex-col w-full items-start">
-                    <div className="bar"><div className="title">标题</div><div className=" w-full"><input id="title" className=" w-full" type="text" /></div></div>
+                <form className="flex flex-col w-full gap-6">
+                    <div className="bar">
+                        <label className="title">标题</label>
+                        <div className="w-full"><input id="title" className="w-full border border-gray-400 round px-2 py-1 text-gray-700" type="text" autoComplete="off" /></div>
+                    </div>
+                    <div className="bar items-start">
+                        <label className="title">简介</label>
+                        <div className="w-full"><textarea id="description" rows={4} className="w-full border border-gray-400 round px-2 py-1 text-gray-700" autoComplete="off" /></div>
+                    </div>
+                    <div className="bar items-start">
+                        <label className="title">标签</label>
+                        <div className="w-full">
+                            <ReactTags
+                                tags={tags}
+                                suggestions={suggestions}
+                                delimiters={delimiters}
+                                handleDelete={handleDelete}
+                                handleAddition={handleAddition}
+                                handleDrag={handleDrag}
+                                handleTagClick={handleTagClick}
+                                inputFieldPosition="bottom"
+                                minQueryLength={1}
+                            />
+                        </div>
+                    </div>
                 </form>
+                <div className="w-full h-12"></div>
             </div>
         </main>
     )
