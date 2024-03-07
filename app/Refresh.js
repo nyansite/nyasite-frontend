86bdc5d6450d3e3264c3e785579ce1cca449090c
flyingsnoopy <lijiaqi1835@outlook.com>
2024-03-07 13:16:32 +0800

    remake refresh

--- app/Refresh.js
+++ app/Refresh.js
@@ -1,20 +1,23 @@
 "use client"
-import { useRouter,usePathname } from "next/navigation"
-export default function Refresh(){
+import { useRouter, usePathname } from "next/navigation"
+import { useEffect } from "react"
+export default function Refresh() {
     const pathname = usePathname()
-    if(pathname == "/login" && pathname == "/register"){
+    if (pathname == "/login" && pathname == "/register") {
         return null
     }
     const router = useRouter()
-    const refresh = async() => {
-        const res = await fetch("/api/refresh",{
-            credentials:"include"
-        })
-        switch(res.status){
-            case 401:
-                router.replace("/login")
+    useEffect(() => {
+        const refresh = async () => {
+            const res = await fetch("/api/refresh", {
+                credentials: "include"
+            })
+            switch (res.status) {
+                case 401:
+                    router.replace("/login")
+            }
         }
-    }
-    refresh()
-    return <div></div>
+        refresh()
+    }, [])
+    return <main></main>
 }
--- app/Sidebar.js
+++ app/Sidebar.js
@@ -19,19 +19,27 @@ async function AvatarBar() {
 	const res = await fetch("http://localhost:8000/api/user_status", {
 		headers: get_header()
 	})
-	if (res.status == 200) {
-		const list = await res.json()
-		return (
-			<a className='flex items-center flex-col gap-2 w-32' href="/user/self">
-				<img src={list.avatar} alt='avatar' className=" h-20 w-20 rounded-full" />
-			</a>
-		)
-	} else if (res.status == 401) {
-		return (
-			<div className='flex items-center flex-col gap-2 w-full'>
-				<a href='/login' className="text_b w-32 hover:w-30">登录</a>
-				<a href='/register' className="text_b w-32 hover:w-30">注册</a>
-			</div>
-		)
+	switch (res.status) {
+		case 200:
+			const list = await res.json()
+			return (
+				<a className='flex items-center flex-col gap-2 w-32' href="/user/self">
+					<img src={list.avatar} alt='avatar' className=" h-20 w-20 rounded-full" />
+				</a>
+			)
+		case 401:
+			return (
+				<div className='flex items-center flex-col gap-2 w-full'>
+					<a href='/login' className="text_b w-32 hover:w-30">登录</a>
+					<a href='/register' className="text_b w-32 hover:w-30">注册</a>
+				</div>
+			)
+		default:
+			return (
+				<div className='flex items-center flex-col gap-2 w-full'>
+					<a href='/login' className="text_b w-32 hover:w-30">登录</a>
+					<a href='/register' className="text_b w-32 hover:w-30">注册</a>
+				</div>
+			)
 	}
 }
