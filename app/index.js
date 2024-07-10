e07d49af699ecc4fba145b73d0d3b380813a2904
flyingsnoopy <lijiaqi1835@outlook.com>
2024-07-10 11:30:45 +0800

    fix display bug of index page

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 "use client"
 import { Circle } from "rc-progress"
-import { ArrowLeftIcon, ArrowRightIcon,PlayCircleIcon } from "@heroicons/react/24/outline";
+import { ArrowLeftIcon, ArrowRightIcon, PlayCircleIcon } from "@heroicons/react/24/outline";
 import { useState } from "react";
 
 export function Poster({ Max }) {
@@ -11,13 +11,13 @@ export function Poster({ Max }) {
             if (number == Max) {
                 setNumber(0)
             } else {
-                setNumber(number + 1) 
+                setNumber(number + 1)
             }
         } else {
             if (number == 0) {
                 setNumber(Max)
             } else {
-                setNumber(number -1)
+                setNumber(number - 1)
             }
         }
     }
@@ -27,7 +27,7 @@ export function Poster({ Max }) {
                 src={"/poster/" + String(number) + ".png"}
             />
             <div className="w-44 h-6 flex justify-end items-center gap-2 relative mx-2" style={{ top: "-1.5rem" }}>
-                <Circle percent={(number / Max)*100} strokeWidth={10} strokeColor={"white"} className=" w-4 h-4" />
+                <Circle percent={(number / Max) * 100} strokeWidth={10} strokeColor={"white"} className=" w-4 h-4" />
                 <button className=" w-4 h-4 flex items-center justify-center" onClick={() => nextOrPerious(false)}><ArrowLeftIcon className="w-3 h-3 text-white" /></button>
                 <button className=" w-4 h-4 flex items-center justify-center" onClick={() => nextOrPerious(true)}><ArrowRightIcon className="w-3 h-3 text-white" /></button>
             </div>
@@ -35,51 +35,60 @@ export function Poster({ Max }) {
     )
 }
 
-export function Trending({ trending }){
-    const [type,setType] = useState(0)
-    if(type == 0){
+export function Trending({ trending }) {
+    const [type, setType] = useState(0)
+    if (type == 0) {
         var content = trending.daily.map(i =>
             <div className="flex flex-col h-32 mx-1" key={i.Id}>
                 <a className="h-20 rounded" href={"/video/" + i.Id}>
                     <img src={i.CoverPath} className="h-full rounded" />
                 </a>
                 <a className=" w-full" href={"/video/" + i.Id}>{i.Title}</a>
-                <div className="flex gap-1 w-full text-gray-400 items-center"><a src={"/circle/"+i.Author.Id}>{i.Author.Name}</a><PlayCircleIcon className="h-5 w-5" /><div className=" w-full truncate">{i.Views}</div></div>
+                <div className="flex flex-auto w-full text-gray-400 items-center">
+                    <a src={"/circle/" + i.Author.Id} className="truncate mr-1">{i.Author.Name}</a>
+                    <PlayCircleIcon className="h-3 w-3" /><div className="truncate">{i.Views}</div>
+                </div>
             </div>
         )
-    }else if(type == 1){
+    } else if (type == 1) {
         var content = trending.weekly.map(i =>
             <div className="flex flex-col h-32 mx-1" key={i.Id}>
                 <a className="h-20 rounded" href={"/video/" + i.Id}>
                     <img src={i.CoverPath} className="h-full rounded" />
                 </a>
                 <a className=" w-full" href={"/video/" + i.Id}>{i.Title}</a>
-                <div className="flex gap-1 w-full text-gray-400 items-center"><a src={"/circle/"+i.Author.Id}>{i.Author.Name}</a><PlayCircleIcon className="h-5 w-5" /><div className=" w-full truncate">{i.Views}</div></div>
+                <div className="flex flex-auto w-full text-gray-400 items-center">
+                    <a src={"/circle/" + i.Author.Id} className="truncate mr-1">{i.Author.Name}</a>
+                    <PlayCircleIcon className="h-3 w-3" /><div className="truncate">{i.Views}</div>
+                </div>
             </div>
         )
-    }else if(type == 2){
+    } else if (type == 2) {
         var content = trending.monthly.map(i =>
             <div className="flex flex-col h-32 mx-1" key={i.Id}>
                 <a className="h-20 rounded" href={"/video/" + i.Id}>
                     <img src={i.CoverPath} className="h-full rounded" />
                 </a>
                 <a className=" w-full" href={"/video/" + i.Id}>{i.Title}</a>
-                <div className="flex gap-1 w-full text-gray-400 items-center"><a src={"/circle/"+i.Author.Id}>{i.Author.Name}</a><PlayCircleIcon className="h-5 w-5" /><div className=" w-full truncate">{i.Views}</div></div>
+                <div className="flex flex-auto w-full text-gray-400 items-center">
+                    <a src={"/circle/" + i.Author.Id} className="truncate mr-1">{i.Author.Name}</a>
+                    <PlayCircleIcon className="h-3 w-3" /><div className="truncate">{i.Views}</div>
+                </div>
             </div>
         )
     }
-    return(
+    return (
         <div className="w-auto h-72 flex flex-col justify-start gap-1">
             <div className="h-6 w-full flex gap-4">
                 <div className="text-base font-bold">热门</div>
-                <button className={"h-6 align-middle "+(type==0?"border-b-2":null)} onClick={() => setType(0)}>每日</button>
-                <button className={"h-6 align-middle "+(type==1?"border-b-2":null)} onClick={() => setType(1)}>每周</button>
-                <button className={"h-6 align-middle "+(type==2?"border-b-2":null)} onClick={() => setType(2)}>每月</button>
+                <button className={"h-6 align-middle " + (type == 0 ? "border-b-2" : null)} onClick={() => setType(0)}>每日</button>
+                <button className={"h-6 align-middle " + (type == 1 ? "border-b-2" : null)} onClick={() => setType(1)}>每周</button>
+                <button className={"h-6 align-middle " + (type == 2 ? "border-b-2" : null)} onClick={() => setType(2)}>每月</button>
                 <a className="hyperlink">详情</a>
             </div>
             <div className=" flex flex-wrap h-full overflow-y-hidden gap-1">
                 {content}
             </div>
         </div>
-    )  
+    )
 }
--- app/page.js
+++ app/page.js
@@ -1,5 +1,7 @@
 import { SidebarRight } from "./Sidebar";
 import { headers } from "next/headers";
+import { ArrowLeftIcon, ArrowRightIcon, PlayCircleIcon } from "@heroicons/react/24/outline";
+
 import Refresh from "./Refresh.js"
 import { Poster, Trending } from "./index.js";
 
@@ -17,15 +19,38 @@ export default async function Home() {
 	return (
 		<main className=" flex justify-center">
 			<div className="flex justify-normal w-11/12 border-8 border-transparent">
-				<div className="w-full h-auto flex flex-col">
+				<div className="w-full h-auto flex flex-col gap-6">
 					<div className="w-full flex gap-8">
 						<Poster Max={2} />
-						{/*<Trending trending={trendingData}/>*/}
+						{/*<Trending trending={trendingData} />*/}
 					</div>
+					<VideoDisplay />
 				</div>
 				<SidebarRight />
 				<Refresh />
 			</div>
 		</main>
 	);
+}
+
+async function VideoDisplay() {
+	const res = await fetch("http://localhost:8000/api/get_all_videos", { headers: get_header() })
+	const data = await res.json()
+	var content = data.result.map(i =>
+		<div className="flex flex-col h-32 mx-1" key={i.Id}>
+			<a className="h-20 rounded" href={"/video/" + i.Id}>
+				<img src={i.CoverPath} className="h-full rounded" />
+			</a>
+			<a className=" w-full" href={"/video/" + i.Id}>{i.Title}</a>
+			<div className="flex flex-auto w-full text-gray-400 items-center">
+				<a src={"/circle/" + i.Author.Id} className="truncate mr-1">{i.Author.Name}</a>
+				<PlayCircleIcon className="h-3 w-3" /><div className="truncate">{i.Views}</div>
+			</div>
+		</div>
+	)
+	return (
+		<div className="w-full flex flex-wrap gap-1">
+			{content}
+		</div>
+	)
 }
